a7c24aa58ecbbdde014fe798a1dfed2e8c94a385
harrison-gaviria <harrisonarroyaveg@gmail.com>
2025-12-12 07:50:58 -0500

    feat(): Fix execution of gh page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-// https://vite.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
